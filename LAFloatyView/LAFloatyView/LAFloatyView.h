385f94affbf68bf469593efe9b4e5110d6610b66
Latif Atçı <latifatci@Latifs-Air.home>
2022-05-25 01:23:01 +0300

    add initial project

---
+++ LAFloatyView/LAFloatyView/LAFloatyView.h
@@ -0,0 +1,18 @@
+//
+//  LAFloatyView.h
+//  LAFloatyView
+//
+//  Created by Latif Atçı on 25.05.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LAFloatyView.
+FOUNDATION_EXPORT double LAFloatyViewVersionNumber;
+
+//! Project version string for LAFloatyView.
+FOUNDATION_EXPORT const unsigned char LAFloatyViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LAFloatyView/PublicHeader.h>
+
+
